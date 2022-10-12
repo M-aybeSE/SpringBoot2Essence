93bccb62dd0576142f34ccf8d08736daeee36628
linhx <linhaoxin@youxin.cloud>
2022-10-12 15:11:38 +0800

    PointCut()的几种定义方式

--- 2_env/ch2.aop/src/main/java/com/bee/sample/ch2/conf/AOPConfig.java
+++ 2_env/ch2.aop/src/main/java/com/bee/sample/ch2/conf/AOPConfig.java
@@ -2,9 +2,12 @@ package com.bee.sample.ch2.conf;
 
 import java.util.Arrays;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
 /**
  * Controller 的所有方法在执行前后都会进入functionAccessCheck方法
@@ -29,4 +32,25 @@ public class AOPConfig {
 			throw e;
 		}
 	}
+
+	/**
+	 * 所有用到@Component注解的方法
+	 */
+	@Pointcut("target(org.springframework.stereotype.Component)")
+	public void customAspect() {
+
+	}
+
+/*	@Pointcut("@annotation(tech.rongxin.oryx.cgi.common.aspect.stock.ThirdStockAyncAnnotation)")
+	public void stockAspect() {
+	}
+
+	@Pointcut(value = "execution(public * tech.rongxin.oryx.cgi.*.controller.*.*(..)) ")
+    public void pointcut() {
+    }
+
+	@Before("stockAspect()")
+	public void doBeforeMethod(JoinPoint joinPoint) {
+
+	}*/
 }
